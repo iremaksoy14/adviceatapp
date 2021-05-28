df2ce015cfee5cabf6f794373a14b3875a26bb85
İrem Aksoy <60525860+iremaksoy14@users.noreply.github.com>
2021-05-28 23:17:54 +0300

    okey
    okey

--- adviceat/App.js
+++ adviceat/App.js
@@ -1,5 +1,7 @@
+
+//güncellendi yeni
 import * as React from 'react';
-//degistirildi
+
 import ReactDOM from 'react-dom';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
